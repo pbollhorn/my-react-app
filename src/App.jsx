c30950d308adf1d581300b9a1a20b13a612b83cd
Peter Bollhorn <pbollhorn@gmail.com>
2025-04-24 11:30:36 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import upper, { text1, text2, text3 } from "./file1";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // alert(upper(text3));
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Ex 1</h2>
+      <p>hallo</p>
+      <p>upper("please uppercase me")</p>
+      <p>{upper("please uppercase me")}</p>
+      <p>{text1}</p>
+      <p>{text2}</p>
+      <p>{text3}</p>
+      <p>{upper(text3)}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
